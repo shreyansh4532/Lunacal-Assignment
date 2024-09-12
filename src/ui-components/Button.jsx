15f29560c1162fc5b5ebbd3647616fe50b0256a4
Shreyansh Mishra <shreyanshmishracr7@gmail.com>
2024-09-12 19:22:25 +0530

    fixed button styling in navbar

--- src/ui-components/Button.jsx
+++ src/ui-components/Button.jsx
@@ -1,8 +1,14 @@
 //eslint-disable-next-line
-function Button({ children, onClick }) {
+function Button({ children, onClick, isActive }) {
   return (
-    <button className="border-none py-2 px-6 focus:bg-[#242424] focus:text-white active:scale-125 focus:scale-125 text-gray-400 font-medium hover:text-white duration-200 transition-all rounded-2xl focus:shadow-darker-enh active:shadow-darker-enh active:darker-enh focus:darker-enh"
-    onClick={onClick}>
+    <button
+      className={`border-none py-2 px-6 transition-all duration-200 rounded-2xl font-medium ${
+        isActive
+          ? "bg-[#242424] text-white scale-125 shadow-darker-enh"
+          : "text-gray-400 hover:text-white"
+      }`}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
--- src/ui-components/NavMenu.jsx
+++ src/ui-components/NavMenu.jsx
@@ -1,11 +1,11 @@
 import Button from "@/ui-components/Button";
 //eslint-disable-next-line
-function NavMenu({ setActive }) {
+function NavMenu({ currActive, setActive }) {
   return (
     <div className="bg-black flex gap-5 rounded-3xl w-[540px] h-[60px] p-2 items-center justify-around">
-      <Button onClick={() => setActive("about")}>About Me</Button>
-      <Button onClick={() => setActive("exp")}>Experience</Button>
-      <Button onClick={() => setActive("recom")}>Recommend</Button>
+      <Button isActive={currActive === "about"} onClick={() => setActive("about")}>About Me</Button>
+      <Button isActive={currActive === "exp"} onClick={() => setActive("exp")}>Experience</Button>
+      <Button isActive={currActive === "recom"} onClick={() => setActive("recom")}>Recommend</Button>
     </div>
   );
 }
